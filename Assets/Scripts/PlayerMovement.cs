8b1ad1f2abdd1f82754395bd5a79b1b9d9ccaa33
Felimia <ss0915625553@gmail.com>
2024-10-06 16:53:59 +0800

    Raycast

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -74,5 +75,25 @@ public class PlayerMovement : MonoBehaviour
         // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump, roll);
         jump = false;
+
+        //Raycast detection
+        float angle = transform.eulerAngles.z;
+        Vector2 rayDirection = new Vector2(Mathf.Cos((angle - 90) * Mathf.Deg2Rad), Mathf.Sin((angle - 90) * Mathf.Deg2Rad));
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, rayDirection, 1.0f, 1 << 0);
+        if (ray.collider)
+        {
+            Debug.DrawRay(transform.position, rayDirection * 1.0f, Color.red, 0, true);
+            Debug.Log(ray.transform.name);
+
+            Vector2 groundNormal = ray.normal;
+            float normalangle = Mathf.Atan2(groundNormal.y, groundNormal.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, normalangle - 90);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, rayDirection * 1.0f, Color.blue, 0, true);
+            Debug.Log("No hit detected.");
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
