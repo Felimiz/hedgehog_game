97c02652b4f34485fbe400e71653c257b8ba99ad
Felimia <ss0915625553@gmail.com>
2025-03-16 16:33:33 +0800

    movefix part2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private Collider2D m_RollDisableCollider;                  // A collider that will be disabled when rolling(Along with m_CrouchDisableCollider)
     [SerializeField] private CircleCollider2D RollingCollider;                // A collider that will be enabled when rolling
 
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    const float k_GroundedRadius = .4f; // Radius of the overlap circle to determine if grounded
     private bool m_Grounded;            // Whether or not the player is grounded.
     const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     private Rigidbody2D m_Rigidbody2D;
@@ -93,11 +93,11 @@ public class CharacterController2D : MonoBehaviour
             IsFalling = false;
         }
 
-        if (movement.rayhit && !wasGrounded)
+        if (movement.rayhit && !m_Grounded)
         {
             m_adhesionForce = adhesionForceB;
         }
-        else if (movement.rayhit && wasGrounded)
+        else if (movement.rayhit && m_Grounded)
         {
             m_adhesionForce = adhesionForceA;
         }
@@ -108,8 +108,8 @@ public class CharacterController2D : MonoBehaviour
 
         //Debug.Log(m_adhesionForce);
 
-        /*
-        if (wasGrounded)
+        
+        if (m_Grounded)
         {
             Debug.Log("Grounded");
         }
@@ -117,7 +117,7 @@ public class CharacterController2D : MonoBehaviour
         {
             Debug.Log("UnGrounded");
         }
-        */
+        
     }
     public void Move(float move, bool crouch, bool puff, bool roll)
     {
